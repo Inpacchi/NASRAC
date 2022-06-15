69b7e17335b3e6e4a9a31db3f22367b307de1d73
Yovarni Yearwood <yovarni@hawser.org>
2022-06-15 01:16:59 -0400

    Finish home page skeleton

---
+++ UI/src/app/models/race.ts
@@ -0,0 +1,6 @@
+export interface Race {
+  trackName: string;
+  raceName: string;
+  date: string;
+  url: string;
+}
--- UI/src/app/pages/home/home.component.ts
+++ UI/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {OwlOptions} from 'ngx-owl-carousel-o';
+import {Race} from '../../models/race';
 
 @Component({
   selector: 'app-home',
@@ -15,9 +17,74 @@ export class HomeComponent implements OnInit {
   public premierSeriesName = 'Kraken Automotive NASRAC Cup Series';
   public seasonType = 'Regular Season';
   public raceName = 'Busch Light Clash at the Coliseum'
-  public raceLogoUrl = 'assets/race-logos/2022/busch_light_clash.png';
+  public raceLogoUrl = 'assets/race-logos/2022/busch-light-clash.png';
+  public previousRaceName = 'NASRAC Championship Weekend @ Phoenix'
+  public previousRaceLogoUrl = 'assets/race-logos/2021/phoenix-championship.png';
+  public firstPlaceDriver = 'Yovarni Yearwood';
+  public secondPlaceDriver = 'Stefan Baggoo';
+  public thirdPlaceDriver = 'Josh Sapienza';
   public currentRaceNumber: number = 1;
   public totalRaces: number = 36;
+  public raceSchedule: Array<Race> = [
+    {
+      trackName: "Daytona International Speedway",
+      raceName: "Bluegreen Vacations Duel",
+      date: "February 11, 2022",
+      url: "assets/race-logos/2021/bluegreen-vacations-duel.jpeg"
+    },
+    {
+      trackName: "Daytona International Speedway",
+      raceName: "Daytona 500",
+      date: "February 14, 2022",
+      url: "assets/race-logos/2021/daytona-500.jpeg"
+    },
+    {
+      trackName: "Daytona International Speedway",
+      raceName: "O'Reilly Auto Parts 253",
+      date: "February 21, 2022",
+      url: "assets/race-logos/2021/o-reilly-auto-parts-253.png"
+    },
+    {
+      trackName: "Homestead-Miami Speedway",
+      raceName: "Dixie Vodka 400",
+      date: "February 28, 2022",
+      url: "assets/race-logos/2021/dixie-vodka-400.png"
+    },
+    {
+      trackName: "Las Vegas Motor Speedway",
+      raceName: "Pennzoil 400",
+      date: "March 7, 2022",
+      url: "assets/race-logos/2021/pennzoil-400.png"
+    },
+    {
+      trackName: "Phoenix Raceway",
+      raceName: "Ruoff Mortgage 500",
+      date: "March 14, 2022",
+      url: "assets/race-logos/2021/ruoff-mortgage-500.png"
+    }
+  ]
+  public carouselOptions: OwlOptions = {
+    autoplay: true,
+    autoHeight: true,
+    center: false,
+    loop: true,
+    items:1,
+    margin: 30,
+    stagePadding: 0,
+    nav: false,
+    dots: true,
+    responsive:{
+      0:{
+        items: 1
+      },
+      600:{
+        items: 2
+      },
+      1000:{
+        items: 3
+      }
+    }
+  }
 
   constructor() { }
 
