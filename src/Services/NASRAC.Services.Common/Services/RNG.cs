bbf7041d1849a67609b2605c2e12c37e959dd418
Yovarni Yearwood <yyearwood@endlessgalaxy.dev>
2023-02-26 19:09:45 -0500

    Add docstrings to RNG Service and fix divide by zero error

--- src/Services/NASRAC.Services.Common/Services/RNG.cs
+++ src/Services/NASRAC.Services.Common/Services/RNG.cs
@@ -9,21 +9,44 @@ public static class RNG
         RandomNumberGenerator = new Random();
     }
     
+    /// <summary>
+    /// Rolls a random double divided by 10 (range: 0.0 - 0.1)
+    /// </summary>
+    /// <returns></returns>
     public static double RollDoubleTenths()
     {
-        return RandomNumberGenerator.NextDouble() / 10;
+        var randomNumber = RandomNumberGenerator.NextDouble();
+        if (randomNumber != 0) return randomNumber / 10;
+        return randomNumber;
     }
 
+    /// <summary>
+    /// Rolls a random integer in the given range (range: lowerBound - upperBound, e.g 0 - 10)
+    /// </summary>
+    /// <param name="lowerBound">Inclusive lowest number to return</param>
+    /// <param name="upperBound">Exclusive highest number to return</param>
+    /// <returns></returns>
     public static int RollIntRange(int lowerBound, int upperBound)
     {
         return RandomNumberGenerator.Next(lowerBound, upperBound);
     }
 
+    /// <summary>
+    /// Rolls a random double in the given range (range: lowerBound - upperBound; e.g 0.0 - 10.0)
+    /// </summary>
+    /// <param name="lowerBound">Inclusive lowest number to return</param>
+    /// <param name="upperBound">Exclusive highest number to return</param>
+    /// <returns></returns>
     public static double RollDoubleRange(double lowerBound, double upperBound)
     {
         return RandomNumberGenerator.NextDouble() * (upperBound - lowerBound) + lowerBound;
     }
 
+    /// <summary>
+    /// Rolls a random integer from the given list
+    /// </summary>
+    /// <param name="list">List of integers to return a random interger from</param>
+    /// <returns></returns>
     public static int RollFromList(List<int> list)
     {
         return list[RandomNumberGenerator.Next(list.Count)];
