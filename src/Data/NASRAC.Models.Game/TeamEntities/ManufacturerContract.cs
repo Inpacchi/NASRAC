d5026a80a0e607ccc7daeea4805da3e4cd943af7
Yovarni Yearwood <yyearwood@endlessgalaxy.dev>
2023-05-04 23:36:28 -0400

    Add Ids for related properties

--- src/Data/NASRAC.Models.Game/TeamEntities/ManufacturerContract.cs
+++ src/Data/NASRAC.Models.Game/TeamEntities/ManufacturerContract.cs
@@ -9,9 +9,27 @@ namespace NASRAC.Models.Game.TeamEntities;
 /// </summary>
 public class ManufacturerContract : BaseContract
 {
+    /// <summary>
+    /// ID for the team associated with the contract
+    /// </summary>
+    [Required]
+    public int TeamId { get; set; }
+    
+    /// <summary>
+    /// Team associated with the contract
+    /// </summary>
     [Required]
     public Team Team { get; set; }
     
+    /// <summary>
+    /// ID for the manufacturer associated with the contract
+    /// </summary>
+    [Required]
+    public int ManufacturerId { get; set; }
+    
+    /// <summary>
+    /// Manufacturer associated with the contract
+    /// </summary>
     [Required]
     public Manufacturer Manufacturer { get; set; }
 }
